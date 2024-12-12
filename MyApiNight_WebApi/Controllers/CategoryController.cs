6e865e590695f84b8afa5530c0803e9e9293ba86
Elif Yıldırım <elifyildirim3@outlook.com>
2024-12-12 20:54:33 +0300

    Ders 27

--- MyApiNight_WebApi/Controllers/CategoryController.cs
+++ MyApiNight_WebApi/Controllers/CategoryController.cs
@@ -1,8 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyApiNight_BusinessLayer.Abstract;
+using MyApiNight_EntityLayer.Concrete;
 
 namespace MyApiNight_WebApi.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class CategoryController : ControllerBase
 {
+	private readonly ICategoryService _categoryService;
+
+	public CategoryController(ICategoryService categoryService)
+	{
+		_categoryService = categoryService;
+	}
+
+	[HttpGet]
+	public IActionResult CategoryList()
+	{
+		var values = _categoryService.TGetAll();
+		return Ok(values);
+	}
+
+	[HttpPost]
+	public IActionResult CreateCategory(Category category)
+	{
+		_categoryService.TInsert(category);
+		return Ok("Ekleme Başarılı");
+
+	}
+
+	[HttpDelete]
+	public IActionResult PutCategory(int id)
+	{
+		_categoryService.TDelete(id);
+		return Ok("Silme Başarılı");
+	}
+
+	[HttpPut]
+	public IActionResult UpdateCategory(Category category)
+	{
+		_categoryService.TUpdate(category);
+		return Ok("Güncelleme Yapıldı..");
+	}
+	[HttpGet("GetCategory")]
+	public IActionResult GetCategory(int id)
+	{
+		var value = _categoryService.TGetById(id);
+		return Ok(value);
+	}
 }
--- MyApiNight_WebApi/Program.cs
+++ MyApiNight_WebApi/Program.cs
@@ -1,6 +1,21 @@
+using MyApiNight_BusinessLayer.Abstract;
+using MyApiNight_BusinessLayer.Concrete;
+using MyApiNight_DataAccessLayer.Abstract;
+using MyApiNight_DataAccessLayer.Context;
+using MyApiNight_DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+
+builder.Services.AddDbContext<ApiContext>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
